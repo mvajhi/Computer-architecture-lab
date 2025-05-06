3eb41a8142b9a751b6a375820eb1f7bc01a36083
mvajhi <mvajhimv@gmail.com>
2025-05-06 17:48:18 +0330

    Refactor memory address calculation for correct memory mapping

--- ARM.srcs/sources_1/new/memory_componets.v
+++ ARM.srcs/sources_1/new/memory_componets.v
@@ -4,6 +4,7 @@ module memory(
     output [31:0] mem_result
 );
 // module implementation goes here
+    wire [31:0] mem_addr = address - 32'd1024; // Adjust address for memory mapping
     reg [31:0] memory [0:64]; // 1KB memory
     initial begin
         $readmemh("data_memory.txt", memory);
@@ -11,9 +12,9 @@ module memory(
 
     always @(posedge clk) begin
         if (memwrite) begin
-            memory[address[11:2]] <= data;
+            memory[mem_addr[11:2]] <= data;
         end
     end
 
-    assign mem_result = memread ? memory[address[11:2]] : 32'bz;
+    assign mem_result = memread ? memory[mem_addr[11:2]] : 32'bz;
 endmodule
